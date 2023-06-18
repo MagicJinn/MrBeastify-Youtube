e4016937ff86e5323f97d0453b13b55b163769e5
MagicJinn <jinn.bz@mail.com>
2023-06-18 13:50:51 +0200

    removed code that requires the youtube API and made extension work on all brow

--- mrbeastify.js
+++ mrbeastify.js
@@ -1,124 +1,53 @@
 const processedThumbnails = []
 const failedThumbnails = []
 
+function applyOverlayToThumbnails() {
+    // Define the function to apply the overlay
+    function applyOverlay(thumbnailElement, overlayImageUrl) {
+        // Create a new img element for the overlay
+        const overlayImage = document.createElement('img');
+        overlayImage.src = overlayImageUrl;
+        overlayImage.style.position = 'absolute';
+        overlayImage.style.top = '0';
+        overlayImage.style.width = '100%';
+        overlayImage.style.height = '100%';
 
+        // Append the overlay to the parent of the thumbnail
+        thumbnailElement.parentElement.appendChild(overlayImage);
+    }
 
-// Fetch YouTube video thumbnails and apply image overlay
-function fetchAndModifyThumbnails() {
+    // Get overlay image URL from your directory
+    
+    // Query all YouTube video thumbnails on the page
     const thumbnailElements = document.querySelectorAll('#thumbnail');
+    
+    // Apply overlay to each thumbnail
     thumbnailElements.forEach((thumbnailElement) => {
-        try {
-            const videoURL = thumbnailElement.href;
-            const videoId = getVideoIdFromUrl(videoURL);
-            if (!processedThumbnails.includes(videoId) && videoURL !== "") {
-                fetchThumbnail(videoId)
-                    .then((thumbnailUrl) => {
-                        const overlayImageUrl = getRandomImageFromDirectory();
-                        applyOverlayToThumbnail(thumbnailUrl, overlayImageUrl, thumbnailElement);
-                    })
-                    .catch(() => {
-                        const overlayImageUrl = getRandomImageFromDirectory();
-                        thumbnailUrl = browser.extension.getURL('images/whiteground.png');
-                        applyOverlayToThumbnail(thumbnailUrl, overlayImageUrl, thumbnailElement);
-                    });
-            }
-        } catch (error) {
-            if (!failedThumbnails.includes(thumbnailElement)) {
-                console.log(error);
-                console.log((thumbnailElement.href || "<empty string>") + " is not a valid URL, retrying.");
-                failedThumbnails.push(thumbnailElement);
-            }
+        if (!processedThumbnails.includes(thumbnailElement)) {
+            const overlayImageUrl = getRandomImageFromDirectory();
+            applyOverlay(thumbnailElement, overlayImageUrl);
+            processedThumbnails.push(thumbnailElement);
         }
     });
 }
 
-// Apply overlay to thumbnail
-function applyOverlayToThumbnail(thumbnailUrl, overlayImageUrl, thumbnailElement) {
-    const overlayImage = new Image();
-    overlayImage.crossOrigin = 'anonymous';
-    overlayImage.src = overlayImageUrl;
-  
-    overlayImage.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = overlayImage.width;
-      canvas.height = overlayImage.height;
-  
-      const context = canvas.getContext('2d');
-  
-      if (thumbnailUrl !== '') {
-        const thumbnailImage = new Image();
-        thumbnailImage.crossOrigin = 'anonymous';
-        thumbnailImage.src = thumbnailUrl;
-  
-        thumbnailImage.onload = () => {
-          context.drawImage(thumbnailImage, 0, 0);  // Draw the thumbnailImage as the background
-          context.drawImage(overlayImage, 0, 0);    // Draw the overlayImage on top
-          const modifiedThumbnailUrl = canvas.toDataURL();
-          thumbnailElement.querySelector('img').src = modifiedThumbnailUrl;
-        };
-      } else {
-        context.drawImage(overlayImage, 0, 0);      // Draw only the overlayImage
-        const modifiedThumbnailUrl = canvas.toDataURL();
-        thumbnailElement.querySelector('img').src = modifiedThumbnailUrl;
-      }
-    };
-  }
-  
-  
-  
-
-// Get video ID from YouTube video URL
-function getVideoIdFromUrl(url) {
-    const urlParams = new URLSearchParams(new URL(url).search);
-    return urlParams.get('v');
-}
-
-// Fetch thumbnail URL using YouTube Data API
-function fetchThumbnail(videoId) {
-    const apiKey = 'AIzaSyCLvkzKtkqX-p-XuPlW8oI6Y9z1wNqzkG8';
-    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
-    console.log(apiUrl)
-    processedThumbnails.push(videoId) // Adds video to the list of processed thumbnails
-    return fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const thumbnailUrl = data.items[0].snippet.thumbnails.default.url;
-            console.log('Thumbnail URL:', thumbnailUrl);
-            return thumbnailUrl; // Resolve the promise with the thumbnail URL
-        })
-        .catch(() => {
-            console.log('Error fetching video details: ' + apiUrl);
-            return Promise.reject(); // Reject the promise without an error message
-        });
-}
-
-
 // Get a random image URL from a directory
 function getRandomImageFromDirectory() {
-    const images = ['1.png', '2.png', '3.png',"4.png"];
+    const images = ['1.png', '2.png', '3.png', '4.png'];
     const randomIndex = Math.floor(Math.random() * images.length);
-    return browser.extension.getURL('images/' + images[randomIndex]);
-}
-
-// Save image locally
-async function saveImageLocally(imageUrl, filename) {
-    try {
-        const response = await fetch(imageUrl);
-        const blob = await response.blob();
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = filename;
-        a.click();
-        URL.revokeObjectURL(url);
-    } catch (error) {
-        console.error('Error saving image:', error);
+    if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.getURL) {
+      // Chrome or Edge
+      return chrome.runtime.getURL('images/' + images[randomIndex]);
+    } else if (typeof browser !== 'undefined' && browser.extension && browser.extension.getURL) {
+      // Firefox
+      return browser.extension.getURL('images/' + images[randomIndex]);
     }
-}
-
+  }
+  
 
 setInterval(function () {
-    fetchAndModifyThumbnails();
+    // fetchAndModifyThumbnails();
+    applyOverlayToThumbnails()
 }, 1000);
 
-console.log("File Loaded Successfully");
+console.log("MrBeastify Loaded Successfully");
