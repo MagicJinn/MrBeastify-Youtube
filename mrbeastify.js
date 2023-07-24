49b8213dd26875a33d394ff642d863cb68e2f906
JxxIT <110342008+JxxIT@users.noreply.github.com>
2023-07-24 11:53:10 +0200

    Optimised the code a bit, so it's shorter.

--- mrbeastify.js
+++ mrbeastify.js
@@ -4,36 +4,35 @@ var flipBlacklist
 
 // Apply the overlay
 function applyOverlay(thumbnailElement, overlayImageURL, flip = false) {
-  // Create a new img element for the overlay
-  const overlayImage = document.createElement("img");
-  overlayImage.src = overlayImageURL;
-  overlayImage.style.position = "absolute";
-  overlayImage.style.top = "0";
-  overlayImage.style.left = "0";
-  overlayImage.style.width = "100%";
-  overlayImage.style.height = "100%";
-  overlayImage.style.zIndex = "0"; // Ensure overlay is on top but below the time indicator
-  if (flip) {
-    overlayImage.style.transform = "scaleX(-1)"; // Flip the image horizontally
+  if (thumbnailElement.nodeName == "IMG") {
+    // Create a new img element for the overlay
+    const overlayImage = document.createElement("img");
+    overlayImage.src = overlayImageURL;
+    overlayImage.style.position = "absolute";
+    overlayImage.style.top = "0";
+    overlayImage.style.left = "0";
+    overlayImage.style.width = "100%";
+    overlayImage.style.height = "100%";
+    overlayImage.style.zIndex = "0"; // Ensure overlay is on top but below the time indicator
+    if (flip) {
+      overlayImage.style.transform = "scaleX(-1)"; // Flip the image horizontally
+    }
+    thumbnailElement.style.position = "relative"; // Style the thumbnailElement to handle absolute positioning
+    thumbnailElement.parentElement.appendChild(overlayImage);
+  } else if (thumbnailElement.nodeName == "DIV") {
+    thumbnailElement.style.backgroundImage = `url("${overlayImageURL}"), ` + thumbnailElement.style.backgroundImage;
+    thumbnailElement.style.cssText = thumbnailElement.style.cssText + "background-blend-mode: normal;";
+    thumbnailElement.parentElement.appendChild(overlayImage);
   }
-  thumbnailElement.style.position = "relative"; // Style the thumbnailElement to handle absolute positioning
-
-  // Append the overlay to the parent of the thumbnail
-  thumbnailElement.parentElement.appendChild(overlayImage);
-}
-
-function applyOverlayForEndScreen(thumbnailElement, overlayImageURL, flip = false) {
-  thumbnailElement.style.backgroundImage = `url("${overlayImageURL}"), ` + thumbnailElement.style.backgroundImage;
-  thumbnailElement.style.cssText = thumbnailElement.style.cssText + "background-blend-mode: normal;";
-}
+  };
 
 // Looks for all thumbnails and applies overlay
 function applyOverlayToThumbnails() {
   // Query all YouTube video thumbnails on the page that haven't been processed yet
   // (ignores shorts thumbnails)
-  const elementQuery =
-    "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element)";
-  const thumbnailElements = document.querySelectorAll(elementQuery);
+  const elementQueryThumbnail =
+    "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='chrome-extension:'])";
+  const thumbnailElements = document.querySelectorAll(elementQueryThumbnail);
 
   // Apply overlay to each thumbnail
   thumbnailElements.forEach((thumbnailElement) => {
@@ -57,40 +56,6 @@ function applyOverlayToThumbnails() {
         overlayImageURL = getImageURL(overlayImageIndex);
       }
       applyOverlay(thumbnailElement, overlayImageURL, flip);
-    }
-  });
-}
-
-function applyOverlayToEndScreenThumbnails() {
-  // Query all YouTube video thumbnails on the page that haven't been processed yet
-  // (ignores shorts thumbnails)
-  
-  const elementQuery =
-    ".ytp-videowall-still-image:not([style*='chrome-extension:'])";
-  const thumbnailElements = document.querySelectorAll(elementQuery);
-
-  // Apply overlay to each thumbnail
-  thumbnailElements.forEach((thumbnailElement) => {
-    // Apply overlay and add to processed thumbnails
-    let loops = Math.random() > 0.001 ? 1 : 20; // Easter egg
-
-    for (let i = 0; i < loops; i++) {
-      // Get overlay image URL from your directory
-      const overlayImageIndex = getRandomImageFromDirectory();
-      let flip = Math.random() < 0.25; // 25% chance to flip the image
-      let overlayImageURL
-      if (flip && flipBlacklist.includes(overlayImageIndex)) {
-        if (useAlternativeImages) {
-          overlayImageURL = getImageURL(`textFlipped/${overlayImageIndex}`);
-          flip = false;
-        } else {
-          overlayImageURL = getImageURL(overlayImageIndex);
-          flip = false;
-        }
-      } else {
-        overlayImageURL = getImageURL(overlayImageIndex);
-      }
-      applyOverlayForEndScreen(thumbnailElement, overlayImageURL, flip);
     }
   });
 }
@@ -189,7 +154,6 @@ GetFlipBlocklist()
 getHighestImageIndex()
   .then(() => {
     setInterval(applyOverlayToThumbnails, 100);
-    setInterval(applyOverlayToEndScreenThumbnails, 100);
     console.log(
       "MrBeastify Loaded Successfully, " + highestImageIndex + " images detected. " + blacklistStatus
     );
